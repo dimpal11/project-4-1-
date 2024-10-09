a15a0f561299812a03577589c1b0b5b7db9bd86e
dimpal11 <161323087+dimpal11@users.noreply.github.com>
2024-10-09 18:26:20 +0530

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+// const menuBtn = document.getElementById('menu-btn');
+// const mobileMenu = document.getElementById('mobile-menu');
+
+// menuBtn.addEventListener('click', () => {
+//   mobileMenu.classList.toggle('hidden'); // Toggle the visibility of the mobile menu
+// });
+
+// const navDialog = document.getElementById('nav-dialog');
+// function handleMenu() {
+//   navDialog.classList.toggle('hidden');
+// }
+
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
